26d79c77477e9847a83aeb56900c63467c2e11f0
Jeremias <jdossantospinheiro20@gmail.com>
2020-11-30 21:02:19 -0300

    coloquei links em alguns endpoints usando hateoas(recuperar todos os clientes,recuperar em especifico) .

--- src/main/java/com/clients/service/ClientService.java
+++ src/main/java/com/clients/service/ClientService.java
@@ -8,7 +8,6 @@ import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import java.util.List;
 
 
@@ -43,8 +42,16 @@ public class ClientService implements AbstractService<Client>{
 
     @Override
     public Client saveClient( Client client) {
-        System.out.println("Saved customer !");
-        return repository.save(client);
+        if (validCPF(client.getCpf()) == true){
+            System.out.println("Saved customer !");
+            return repository.save(client);
+
+        }else {
+            System.out.println("unable to save customer");
+            return null;
+        }
+
+
 
     }
 
@@ -77,6 +84,45 @@ public class ClientService implements AbstractService<Client>{
         return search;
     }
 
+    protected boolean validCPF(String cpf){
 
+        int iDigito1Aux = 0, iDigito2Aux = 0, iDigitoCPF;
+        int iDigito1 = 0, iDigito2 = 0, iRestoDivisao = 0;
+        String strDigitoVerificador, strDigitoResultado;
+
+        if (!cpf.substring(0,1).equals("")){
+            try{
+                cpf = cpf.replace('.',' ');
+                cpf = cpf.replace('-',' ');
+                cpf = cpf.replaceAll(" ","");
+                for (int iCont = 1; iCont < cpf.length() -1; iCont++) {
+                    iDigitoCPF = Integer.valueOf(cpf.substring(iCont -1, iCont)).intValue();
+                    iDigito1Aux = iDigito1Aux + (11 - iCont) * iDigitoCPF;
+                    iDigito2Aux = iDigito2Aux + (12 - iCont) * iDigitoCPF;
+                }
+                iRestoDivisao = (iDigito1Aux % 11);
+                if (iRestoDivisao < 2) {
+                    iDigito1 = 0;
+                } else {
+                    iDigito1 = 11 - iRestoDivisao;
+                }
+                iDigito2Aux += 2 * iDigito1;
+                iRestoDivisao = (iDigito2Aux % 11);
+                if (iRestoDivisao < 2) {
+                    iDigito2 = 0;
+                } else {
+                    iDigito2 = 11 - iRestoDivisao;
+                }
+                strDigitoVerificador = cpf.substring(cpf.length()-2, cpf.length());
+                strDigitoResultado = String.valueOf(iDigito1) + String.valueOf(iDigito2);
+                strDigitoVerificador.equals(strDigitoResultado);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
 }
